ec9f11f8cd05ca23abdfb79239443aa34c41bb9b
Mactavish Carney <maccarney@hotmail.ca>
2025-09-04 14:11:19 -0400

    week 1

--- game/include/game.h
+++ game/include/game.h
@@ -23,5 +23,5 @@ Use this as a starting point or replace it with your code.
 */
 
 
-static constexpr int InitialWidth = 1200;
-static constexpr int InitialHeight = 800;
+static constexpr int InitialWidth = 1000;
+static constexpr int InitialHeight = 600;
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -7,28 +7,56 @@ See documentation here: https://www.raylib.com/, and examples here: https://www.
 #include "raymath.h"
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
+#include "game.h"
 
 const unsigned int TARGET_FPS = 50;
+float dt = 1.0f / TARGET_FPS;
 float time = 0;
+float x = 500;
+float y = 500;
+float frequency = 1;
+float amplitude = 100;
+
+void update()
+{
+	dt = 1.0f / TARGET_FPS;
+	time += dt;
+
+	x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
+	y = y + (cos(time * frequency)) * frequency * amplitude * dt;
+
+}
+void draw()
+{
+	BeginDrawing();
+	ClearBackground(BLACK);
+	DrawText("Mactavish Carney 101534351", 10, float(GetScreenHeight() - 25), 20, LIGHTGRAY);
+
+
+
+	GuiSliderBar(Rectangle{ 10, 10, 800, 20 }, "", TextFormat("%.2f", time), &time, 0, 240);
+	GuiSliderBar(Rectangle{ 10, 30, 800, 20 }, "", TextFormat("%.2f", frequency), &frequency, 0, 240);
+	GuiSliderBar(Rectangle{ 10, 50, 800, 20 }, "", TextFormat("%.2f", amplitude), &amplitude, 0, 240);
+	DrawText(TextFormat("T: %3.2f", time), GetScreenWidth() - 150, 5, 30, LIGHTGRAY);
+	DrawText(TextFormat("f: %3.2f", frequency), GetScreenWidth() - 150, 25, 30, LIGHTGRAY);
+	DrawText(TextFormat("a: %3.2f", amplitude), GetScreenWidth() - 150, 45, 30, LIGHTGRAY);
+
+	DrawCircle(x, y, 70, RED);
+
+	EndDrawing();
+
+}
 int main()
 {
-    InitWindow(1200, 800, "Physics-1");
-    SetTargetFPS(TARGET_FPS);
+	InitWindow(InitialWidth, InitialHeight, "Mactavish Carney 101534351 GAME2005");
+	SetTargetFPS(TARGET_FPS);
 
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-            ClearBackground(WHITE);
-            DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
+	while (!WindowShouldClose())
+	{
+		update();
+		draw();
+	}
 
-
-            time += 1;
-            GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
-
-
-        EndDrawing();
-    }
-
-    CloseWindow();
-    return 0;
+	CloseWindow();
+	return 0;
 }
