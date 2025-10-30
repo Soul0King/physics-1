3a1265daf861ebe9bd2d02d369068d8498a1d843
Mactavish Carney <maccarney@hotmail.ca>
2025-10-30 14:11:40 -0400

    fixed LE5 (i think)

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -107,7 +107,7 @@ bool CircleCircleOverlap(FizziksCircle* circleA, FizziksCircle* circleB) {
 		else
 			normalAToB = displacementFromAToB / distance;
 
-		Vector2 normalAToB = (displacementFromAToB / distance);
+		//Vector2 normalAToB = (displacementFromAToB / distance);
 		Vector2 mtv = normalAToB * overlap; // minimum translation vector
 
 		circleA->position -= mtv * 0.5f;
