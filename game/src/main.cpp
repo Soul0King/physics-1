eb126dc07cf125b719bde4b15b3daa1bccc73a13
Mactavish-Carney <maccarney@hotmail.ca>
2025-11-27 14:22:51 -0500

    LE7 done

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -17,6 +17,26 @@ float time = 0;
 
 float restitution = 0.9f;
 
+// LE7 slider variables
+float rCircleMass = 1;
+float rCircleSpeed = 1;
+Vector2 rCircleNormal = { 0, 1 };
+
+float gCircleMass = 1;
+float gCircleSpeed = 1;
+Vector2 gCircleNormal = { 0, 1 };
+
+float bCircleMass = 1;
+float bCircleSpeed = 100;
+Vector2 bCircleNormal = { 1, 0 };
+
+float pCircleMass = 1;
+float pCircleSpeed = 1;
+Vector2 pCircleNormal = { 0, 1 };
+
+float yCircleMass = 1;
+float yCircleSpeed = 1;
+
 
 enum FizziksShape {
 	CIRCLE,
@@ -212,11 +232,65 @@ float startY = 500;
 
 FizziksWorld world;
 FizziksHalfspace halfspace;
-//FizziksHalfspace halfspace2;
-FizziksCircle* rCircle = new FizziksCircle();
-FizziksCircle* gCircle = new FizziksCircle();
-FizziksCircle* bCircle = new FizziksCircle();
-FizziksCircle* yCircle = new FizziksCircle();
+
+
+void MakeDeleteableObjekts() {
+	
+	FizziksCircle* rCircle = new FizziksCircle();
+	FizziksCircle* gCircle = new FizziksCircle();
+	FizziksCircle* bCircle = new FizziksCircle();
+	FizziksCircle* yCircle = new FizziksCircle();
+	FizziksCircle* pCircle = new FizziksCircle();
+
+	rCircle->mass = rCircleMass;
+	rCircle->bounciness = restitution;
+	rCircle->radius = 20;
+	rCircle->coefficientOfFriction = 0.1;
+	rCircle->color = RED;
+	rCircle->baseColor = RED;
+	rCircle->position = { 100, 500 };
+	rCircle->velocity = rCircleNormal * rCircleSpeed;
+	world.add(rCircle);
+
+	pCircle->mass = pCircleMass;
+	pCircle->bounciness = restitution;
+	pCircle->radius = 20;
+	pCircle->coefficientOfFriction = 0.1;
+	pCircle->color = PURPLE;
+	pCircle->baseColor = PURPLE;
+	pCircle->position = { 100, 480 };
+	pCircle->velocity = pCircleNormal * pCircleSpeed;
+	world.add(pCircle);
+
+	gCircle->mass = gCircleMass;
+	gCircle->bounciness = restitution;
+	gCircle->coefficientOfFriction = 0.8;
+	gCircle->color = GREEN;
+	gCircle->baseColor = GREEN;
+	gCircle->position = { 200, 500 };
+	gCircle->velocity = gCircleNormal * gCircleSpeed;
+	world.add(gCircle);
+
+	bCircle->mass = bCircleMass;
+	bCircle->bounciness = restitution;
+	bCircle->radius = 20;
+	bCircle->coefficientOfFriction = 0.1;
+	bCircle->color = BLUE;
+	bCircle->baseColor = BLUE;
+	bCircle->position = { 600, 680 };
+	bCircle->velocity = bCircleNormal * bCircleSpeed;
+	world.add(bCircle);
+
+	yCircle->mass = yCircleMass;
+	yCircle->bounciness = restitution;
+	yCircle->radius = 20;
+	yCircle->coefficientOfFriction = 0.8;
+	yCircle->color = YELLOW;
+	yCircle->baseColor = YELLOW;
+	yCircle->position = { 800, 680 };
+	yCircle->velocity = { 0, 0 };
+	world.add(yCircle);
+}
 
 
 bool CircleCircleOverlap(FizziksCircle* circleA, FizziksCircle* circleB) {
@@ -313,7 +387,7 @@ bool CircleHalfspaceOverlap(FizziksCircle* circle, FizziksHalfspace* halfspace) 
 		//Vector2 velocityBRelativeToA = circleB->velocity - circleA->velocity;
 		float closingVelocity1D = Vector2DotProduct(circle->velocity, halfspace->getNormal());
 		//if dot is negative then we are coliding. if positive, not colliding
-		if (closingVelocity1D >= 0) return true;
+		if (closingVelocity1D >= -2) return true;
 
 		float restitution = circle->bounciness * halfspace->bounciness;
 		//velFinal = velInitial + -(1 + resitiution * velInitial)
@@ -366,6 +440,23 @@ void update()
 
 		world.add(newBird);
 	}
+	if (IsKeyPressed(KEY_R)) {
+		for (int i = 0; i < world.objekts.size(); i++) {
+
+			FizziksObjekt* objekt = world.objekts[i];
+
+			if (objekt->Shape() != HALF_SPACE)
+			{
+				auto iterator = (world.objekts.begin() + i);
+				FizziksObjekt* pointerToFizziksObjekt = *iterator;
+				delete pointerToFizziksObjekt;
+
+				world.objekts.erase(iterator);
+				i--;
+			}
+		}
+		MakeDeleteableObjekts();
+	 }
 
 	
 
@@ -378,17 +469,17 @@ void draw()
 
 
 
-	GuiSliderBar(Rectangle{ 10, 10, 800, 20 }, "", TextFormat("%.2f", time), &time, 0, 240);
+	GuiSliderBar(Rectangle{ 100, 10, 800, 20 }, "", TextFormat("%.2f", time), &time, 0, 240);
 
-	GuiSliderBar(Rectangle{ 10, 30, 800, 20 }, "Speed", TextFormat("Speed: %.0f", speed), &speed, -1000, 1000);
+	GuiSliderBar(Rectangle{ 100, 30, 800, 20 }, "Speed", TextFormat("Speed: %.0f", speed), &speed, -1000, 1000);
 
-	GuiSliderBar(Rectangle{ 10, 50, 800, 20 }, "Angle", TextFormat("Angle: %.0f Degrees", angle), &angle, -180, 180);
+	GuiSliderBar(Rectangle{ 100, 50, 800, 20 }, "Angle", TextFormat("Angle: %.0f Degrees", angle), &angle, -180, 180);
 
-	GuiSliderBar(Rectangle{ 10, 70, 400, 20 }, "StartPosX", TextFormat("StartPosX: %.0f", startX), &startX, 0, GetScreenWidth());
+	GuiSliderBar(Rectangle{ 100, 70, 400, 20 }, "StartPosX", TextFormat("StartPosX: %.0f", startX), &startX, 0, GetScreenWidth());
 
-	GuiSliderBar(Rectangle{ 600, 70, 400, 20 }, "StartPosY", TextFormat("StartPosY: %.0f", startY), &startY, 0, GetScreenHeight());
+	GuiSliderBar(Rectangle{ 700, 70, 400, 20 }, "StartPosY", TextFormat("StartPosY: %.0f", startY), &startY, 0, GetScreenHeight());
 
-	GuiSliderBar(Rectangle{ 10, 90, 800, 20 }, "Gravity Y", TextFormat("Gravity Y: %.0f Px/sec^2", world.accelerationGravity.y), &world.accelerationGravity.y, -1000, 1000);
+	GuiSliderBar(Rectangle{ 100, 90, 800, 20 }, "Gravity Y", TextFormat("Gravity Y: %.0f Px/sec^2", world.accelerationGravity.y), &world.accelerationGravity.y, -1000, 1000);
 
 	DrawText(TextFormat("T: %3.2f", time), GetScreenWidth() - 150, 5, 30, LIGHTGRAY);
 
@@ -397,18 +488,34 @@ void draw()
 
 	DrawLineEx(startPos, startPos + velocity, 3, RED);
 
-	GuiSliderBar(Rectangle{ 10, 110, 400, 20 }, "halfspace X", TextFormat("X: %.0f", halfspace.position.x), &halfspace.position.x, 0, GetScreenWidth());
-	GuiSliderBar(Rectangle{ 500, 110, 400, 20 }, "Y", TextFormat("Y: %.0f", halfspace.position.y), &halfspace.position.y, 0, GetScreenHeight());
+	GuiSliderBar(Rectangle{ 100, 110, 400, 20 }, "halfspace X", TextFormat("X: %.0f", halfspace.position.x), &halfspace.position.x, 0, GetScreenWidth());
+	GuiSliderBar(Rectangle{ 700, 110, 400, 20 }, "halfspace Y", TextFormat("Y: %.0f", halfspace.position.y), &halfspace.position.y, 0, GetScreenHeight());
 
 	float halfspaceRotation = halfspace.getRotation();
-	GuiSliderBar(Rectangle{ 10, 130, 800, 20 }, "rotation", TextFormat("rotation: %.0f", halfspace.getRotation()), &halfspaceRotation, -360, 360);
+	GuiSliderBar(Rectangle{ 100, 130, 800, 20 }, "rotation", TextFormat("rotation: %.0f", halfspace.getRotation()), &halfspaceRotation, -360, 360);
 	halfspace.setRotationDegrees(halfspaceRotation);
 
 	//control for friction
 	//GuiSliderBar(Rectangle{ 700, 150, 400, 20 }, "u", TextFormat("Y: %.2f", coefficientOfFriction), &coefficientOfFriction, 0, 1);
 
-	//control fo restitution
-	GuiSliderBar(Rectangle{ 700, 150, 400, 20 }, "restitution", TextFormat("Y: %.2f", restitution), &restitution, 0, 1);
+	//control for restitution
+	GuiSliderBar(Rectangle{ 100, 150, 400, 20 }, "restitution", TextFormat("R: %.2f", restitution), &restitution, 0, 1);
+
+	// LE7 controls
+	GuiSliderBar(Rectangle{ 100, 170, 400, 20 }, "Red Circle Mass", TextFormat("M: %.0f", rCircleMass), &rCircleMass, 1, 50);
+	GuiSliderBar(Rectangle{ 700, 170, 400, 20 }, "Red Circle Speed", TextFormat("M: %.0f", rCircleSpeed), &rCircleSpeed, 0, 1000);
+
+	GuiSliderBar(Rectangle{ 100, 190, 400, 20 }, "green Circle Mass", TextFormat("M: %.0f", gCircleMass), &gCircleMass, 1, 50);
+	GuiSliderBar(Rectangle{ 700, 190, 400, 20 }, "green Circle Speed", TextFormat("M: %.0f", gCircleSpeed), &gCircleSpeed, 0, 1000);
+
+	GuiSliderBar(Rectangle{ 100, 210, 400, 20 }, "blue Circle Mass", TextFormat("M: %.0f", bCircleMass), &bCircleMass, 1, 50);
+	GuiSliderBar(Rectangle{ 700, 210, 400, 20 }, "blue Circle Speed", TextFormat("M: %.0f", bCircleSpeed), &bCircleSpeed, 0, 1000);
+
+	GuiSliderBar(Rectangle{ 100, 230, 400, 20 }, "purple Circle Mass", TextFormat("M: %.0f", pCircleMass), &pCircleMass, 1, 50);
+	GuiSliderBar(Rectangle{ 700, 230, 400, 20 }, "purple Circle Speed", TextFormat("M: %.0f", pCircleSpeed), &pCircleSpeed, 0, 1000);
+
+	GuiSliderBar(Rectangle{ 100, 250, 400, 20 }, "yellow Circle Mass", TextFormat("M: %.0f", yCircleMass), &yCircleMass, 1, 50);
+	
 
 
 	for (int i = 0; i < world.objekts.size(); i++) {
@@ -428,37 +535,7 @@ int main()
 	halfspace.position = { 500, 700 };
 	world.add(&halfspace);
 
-	/*rCircle->mass = 2;
-	rCircle->coefficientOfFriction = 0.1;
-	rCircle->color = RED;
-	rCircle->baseColor = RED;
-	rCircle->position = { 100, 500 };
-	world.add(rCircle);
-
-	gCircle->mass = 2;
-	gCircle->coefficientOfFriction = 0.8;
-	gCircle->color = GREEN;
-	gCircle->baseColor = GREEN;
-	gCircle->position = { 200, 500 };
-	world.add(gCircle);*/
-
-	bCircle->mass = 100;
-	bCircle->radius = 30;
-	bCircle->coefficientOfFriction = 0.1;
-	bCircle->color = BLUE;
-	bCircle->baseColor = BLUE;
-	bCircle->position = { 400, 550 };
-	bCircle->velocity = { 100, 0 };
-	world.add(bCircle);
-
-	yCircle->mass = 0.1;
-	yCircle->radius = 30;
-	yCircle->coefficientOfFriction = 0.8;
-	yCircle->color = YELLOW;
-	yCircle->baseColor = YELLOW;
-	yCircle->position = { 800, 550 };
-	yCircle->velocity = { 0, 0 };
-	world.add(yCircle);
+	MakeDeleteableObjekts();
 
 	while (!WindowShouldClose())
 	{
